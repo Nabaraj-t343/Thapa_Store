1736abe252557fff53e8b594b8606bd2c0717fcc
Nabaraj-t343 <sumit.thapa2060@gmail.com>
2025-12-10 21:31:09 +0545

    category

---
+++ route/category.route.js
@@ -0,0 +1,5 @@
+import { Router } from 'express'
+import auth from '../middleware/auth.js'
+import { AddCategoryController, deleteCategoryController, getCategoryController, updateCategoryController } from '../controllers/category.controller.js'
+
+const categoryRouter = Router()
