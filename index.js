1c7a9312c2efa02f73bcdbd624a432f77198135c
Nabaraj-t343 <sumit.thapa2060@gmail.com>
2025-12-17 20:39:25 +0545

    index

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import connectDB from "./config/connectDB.js";
 import userRouter from "./route/user.route.js";
 import categoryRouter from './route/category.route.js'
 import uploadRouter from './route/upload.router.js'
+import subCategoryRouter from './route/subCategory.route.js'
 
 const app = express()
 app.use(cors({
