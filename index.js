8e6b61d13ff197ff7ec2d723e3bb9e2129501f3d
Nabaraj-t343 <sumit.thapa2060@gmail.com>
2025-12-21 20:45:31 +0545

    index

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ app.use("/api/subcategory",subCategoryRouter)
 app.use("/api/product",productRouter)
 app.use("/api/cart",cartRouter)
 app.use("/api/address",addressRouter)
+app.use('/api/order',orderRouter)
 
 connectDB().then(()=>{
 app.listen(PORT,()=>{
