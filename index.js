bfa185cd5d46ac498fd3980b40c1a7aa5525a2d2
Nabaraj-t343 <sumit.thapa2060@gmail.com>
2025-12-22 20:45:55 +0545

    index

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
-import cookieparser from 'cookie-parser'
+import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
-import helmet, { crossOriginResourcePolicy } from 'helmet'
-import connectDB from "./config/connectDB.js";
-import userRouter from "./route/user.route.js";
+import helmet from 'helmet'
+import connectDB from './config/connectDB.js'
+import userRouter from './route/user.route.js'
 import categoryRouter from './route/category.route.js'
 import uploadRouter from './route/upload.router.js'
 import subCategoryRouter from './route/subCategory.route.js'
@@ -17,26 +17,25 @@ import orderRouter from './route/order.route.js'
 
 const app = express()
 app.use(cors({
-    credentials:true,
-    origin: process.env.FRONTEND_URL
-
+    credentials : true,
+    origin : process.env.FRONTEND_URL
 }))
-app.use(express.json());
-app.use(cookieparser())
-app.use(morgan("combined"))
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan())
 app.use(helmet({
-   crossOriginResourcePolicy :false
+    crossOriginResourcePolicy : false
 }))
 
-const PORT = process.env.PORT || 8080;
-
+const PORT = 8080 || process.env.PORT 
 
 app.get("/",(request,response)=>{
     ///server to client
-response.json({
-    message: "server is running " + PORT
+    response.json({
+        message : "Server is running " + PORT
+    })
 })
-})
+
 app.use('/api/user',userRouter)
 app.use("/api/category",categoryRouter)
 app.use("/api/file",uploadRouter)
@@ -47,10 +46,7 @@ app.use("/api/address",addressRouter)
 app.use('/api/order',orderRouter)
 
 connectDB().then(()=>{
-app.listen(PORT,()=>{
-    console.log("server is running",PORT)
+    app.listen(PORT,()=>{
+        console.log("Server is running",PORT)
+    })
 })
-})
-
-
-
